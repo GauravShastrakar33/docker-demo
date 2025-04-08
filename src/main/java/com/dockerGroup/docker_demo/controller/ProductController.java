3ffb44f656943a86294325d34bc772de9bcd99d8
GauravShastrakar33 <33s.gaurav@gmail.com>
2025-04-08 11:39:32 +0530

    docker demo commit

---
+++ src/main/java/com/dockerGroup/docker_demo/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.dockerGroup.docker_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/dockerTest")
+@RestController
+public class ProductController {
+
+    @GetMapping
+    public String getHello(){
+        return "Hello from docker-demo";
+    }
+
+
+}
